a219f434683b22bcd5aa0c136dc6fc61b5cd8471
Volodymyr Yeikovych <yeikovychvolodymyrwork@gmail.com>
2022-05-27 23:17:54 +0200

    Removed unused exceptionWrapper.

--- src/main/java/v1/exceptions/InterruptedExceptionWrapper.java
+++
@@ -1,7 +0,0 @@
-package v1.exceptions;
-
-public class InterruptedExceptionWrapper extends RuntimeException{
-    public InterruptedExceptionWrapper(Throwable cause) {
-        super(cause);
-    }
-}
